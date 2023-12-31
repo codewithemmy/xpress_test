b4948654fdc4fb75aa0b687a781600037d857caf
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2023-12-31 23:44:23 +0100

    Update app.js

--- core/app.js
+++ core/app.js
@@ -12,13 +12,14 @@ const routes = require("./routes")
 const app = express()
 
 const application = () => {
+  app.use(cors())
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
   app.use(helmet())
   app.use(xss())
   app.use(mongoSanitize())
   app.use(compression())
-  app.use(cors())
+  
   app.use(emailValidation)
 
   app.get("/", (req, res) => {
