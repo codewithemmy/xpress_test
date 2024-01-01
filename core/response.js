a4fc65190e45ef1afa903dc8476ed543abbda810
codewithemmy <codewithemmy@gmail.com>
2024-01-01 06:59:49 +0100

    updated

--- core/response.js
+++ core/response.js
@@ -15,7 +15,7 @@ module.exports.handleApplicationErrors = (err, req, res, _next) => {
     return res.status(statusCode).json(payload)
   }
 
-  res.status(400).json({ message: generalMessages.UNEXPECTED_FAILURE })
+  res.status(400).json({ message: generalMessages.UNEXPECTED_FAILURE, error: err.message })
 }
 
 module.exports.notFound = (req, res) => {
